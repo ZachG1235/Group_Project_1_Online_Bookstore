f9eb4cfd0a01d1c55e45e8c620251869c56f1f29
Devin Jay San Nicolas <dds324@nau.edu>
2025-11-05 15:47:28 -0700

    made database

---
+++ dbsql.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE BookDB;
+CREATE TABLE Users (
+    username VARCHAR(50) PRIMARY KEY,
+    displayName VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+
+);
+
+CREATE TABLE Books (
+    bookID SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    pubDate DATE NOT NULL,
+    rating FLOAT NOT NULL
+);
+
+CREATE TABLE Reviews (
+    reviewID SERIAL PRIMARY KEY,
+    bookID INT REFERENCES Books(bookID),
+    author VARCHAR(50) REFERENCES Users(username),
+    reviewText TEXT NOT NULL,
+    reviewTitle VARCHAR(100) NOT NULL,
+    rating INT NOT NULL,
+    DELETE ON CASCADE
+);
